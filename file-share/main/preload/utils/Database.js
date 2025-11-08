cd606112414f6a221851d1222ead8dc26ae6c303
xiaojianzheng <1272209235@qq.com>
2025-11-08 19:24:19 +0800

    add file-share

---
+++ file-share/main/preload/utils/Database.js
@@ -0,0 +1,25 @@
+// ----- 数据库接口封装 k-v 存储 -----
+function setStorageItem(key, value) {
+  rubick.dbStorage.setItem(key, value);
+}
+
+function getStorageItem(key, defaultValue = null) {
+  let value = rubick.dbStorage.getItem(key);
+  // 存在直接返回
+  if (!!value) {
+    return value;
+  }
+  // 不存在且没有默认值
+  if (defaultValue == null) {
+    return null;
+  }
+  // 使用默认值
+  if (!!defaultValue && typeof defaultValue === "function") {
+    defaultValue = defaultValue();
+  }
+  setStorageItem(key, defaultValue);
+  return defaultValue;
+}
+
+exports.setStorageItem = setStorageItem;
+exports.getStorageItem = getStorageItem;
---
+++ file-share/main/preload/utils/ZipUtil.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const archiver = require("archiver");
+
+/**
+ * @param {String} sourceDir: /some/folder/to/compress
+ * @param {String} outPath: /path/to/created.zip
+ * @returns {Promise}
+ */
+const zipDirectory = (sourceDir, outPath) => {
+  const archive = archiver("zip", { zlib: { level: 1 } });
+  const stream = fs.createWriteStream(outPath);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(sourceDir, false)
+      .on("error", (err) => reject(err))
+      .pipe(stream);
+
+    stream.on("close", () => resolve(outPath));
+    archive.finalize();
+  });
+};
+
+exports.zipDirectory = zipDirectory;
